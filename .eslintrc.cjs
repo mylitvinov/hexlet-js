54dd690129a0b7d9f9cbcbe22d8a5b5b1ef9dbb1
mylitvinov <vinchester30@mail.ru>
2021-08-03 21:02:22 +0300

    add linter

---
+++ .eslintrc.cjs
@@ -0,0 +1,28 @@
+module.exports = {
+	'env': {
+		'es2021': true,
+		'node': true
+	},
+	'extends': 'eslint:recommended',
+	'parserOptions': {
+		'ecmaVersion': 12
+	},
+	'rules': {
+		'indent': [
+			'error',
+			'tab'
+		],
+		'linebreak-style': [
+			'error',
+			'windows'
+		],
+		'quotes': [
+			'error',
+			'single'
+		],
+		'semi': [
+			'error',
+			'always'
+		]
+	}
+};
